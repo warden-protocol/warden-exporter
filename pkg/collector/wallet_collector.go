1af0912ef4bde27e6795c59818b387ffd83afae8
Joonas Lehtimaki <joonas.lehtimaki@gmail.com>
2024-10-25 12:18:07 +0300

    fix: support award and big ints on the walletCollector

--- pkg/collector/wallet_collector.go
+++ pkg/collector/wallet_collector.go
@@ -3,9 +3,11 @@ package collector
 import (
 	"context"
 	"fmt"
+	"math/big"
 	"strings"
 	"time"
 
+	"cosmossdk.io/math"
 	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/warden-protocol/warden-exporter/pkg/config"
@@ -15,6 +17,8 @@ import (
 
 const (
 	walletBalanceMetricName = "cosmos_wallet_balance"
+	award                   = 1e18
+	uward                   = 1e6
 )
 
 //nolint:gochecknoglobals // this is needed as it's used in multiple places
@@ -39,7 +43,12 @@ func (w WalletBalanceCollector) Describe(ch chan<- *prometheus.Desc) {
 }
 
 func (w WalletBalanceCollector) Collect(ch chan<- prometheus.Metric) {
-	var balance uint64
+	var balanceRaw math.Int
+	denomsMap := map[string]float64{
+		"award": award,
+		"uward": uward,
+	}
+
 	ctx, cancel := context.WithTimeout(
 		context.Background(),
 		time.Duration(w.Cfg.Timeout)*time.Second,
@@ -56,15 +65,29 @@ func (w WalletBalanceCollector) Collect(ch chan<- prometheus.Metric) {
 
 	addresses := strings.Split(w.Cfg.WalletAddresses, ",")
 	for _, addr := range addresses {
-		balance, err = client.Balance(ctx, addr, w.Cfg.Denom)
+		balanceRaw, err = client.Balance(ctx, addr, w.Cfg.Denom)
 		if err != nil {
 			log.Error(err.Error())
 			status = errorStatus
 		}
+
+		// Convert balance from atto to none
+		if _, ok := denomsMap[w.Cfg.Denom]; !ok {
+			log.Error("Denom not found")
+			status = errorStatus
+		}
+
+		// Adjust based on your denomination
+		denomFactor := new(
+			big.Float,
+		).SetFloat64(denomsMap[w.Cfg.Denom])
+		balanceBig := new(big.Float).SetInt(balanceRaw.BigInt())
+		balance, _ := new(big.Float).Quo(balanceBig, denomFactor).Float64()
+
 		ch <- prometheus.MustNewConstMetric(
 			walletBalance,
 			prometheus.GaugeValue,
-			float64(balance),
+			balance,
 			[]string{
 				w.Cfg.ChainID,
 				addr,
--- pkg/grpc/grpcBank.go
+++ pkg/grpc/grpcBank.go
@@ -3,18 +3,19 @@ package grpc
 import (
 	"context"
 
+	"cosmossdk.io/math"
 	bank "github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
-func (c Client) Balance(ctx context.Context, address, denom string) (uint64, error) {
+func (c Client) Balance(ctx context.Context, address, denom string) (math.Int, error) {
 	client := bank.NewQueryClient(c.conn)
 
 	req := bank.QueryBalanceRequest{Address: address, Denom: denom}
 
 	balance, err := client.Balance(ctx, &req)
 	if err != nil {
-		return 0, endpointError(err.Error())
+		return math.Int{}, endpointError(err.Error())
 	}
 
-	return balance.Balance.Amount.Uint64(), nil
+	return balance.Balance.Amount, nil
 }
